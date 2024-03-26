9ffd3279382737132c79e7375e30c62302eca843
yigitkalan <sametykalan@gmail.com>
2024-03-26 20:32:38 +0300

    fix: update dto id's

--- backend/planit.Application/DTOs/BoardDto.cs
+++ backend/planit.Application/DTOs/BoardDto.cs
@@ -1,7 +1,7 @@
 namespace planit.Application.DTOs;
 public class BoardDto
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public Guid OwnerId { get; set; }
 }
--- backend/planit.Application/DTOs/ItemDto.cs
+++ backend/planit.Application/DTOs/ItemDto.cs
@@ -2,9 +2,10 @@ namespace planit.Application.DTOs;
 public class ItemDto
 {
 
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public int Order { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public Guid ColumnId { get; set; }
 
 }
